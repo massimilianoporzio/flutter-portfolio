f1986c8b7b27457bf0f857300149ad6aab7ce9b3
Massimiliano Porzio <massimiliano.porzio@gmail.com>
2025-06-17 18:56:46 +0200

    da ufficio ma source code no

---
+++ lib/conf/app_text_styles.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/widgets.dart';
+
+abstract class AppTextStyles {
+  TextStyle get titleSmBold;
+  TextStyle get bodyMdMedium;
+  TextStyle get titleMdMedium;
+  TextStyle get bodyLgBold;
+  TextStyle get bodyLgMedium;
+  TextStyle get titleLgBold;
+}
+
+class SmallTextStyles extends AppTextStyles {
+  @override
+  TextStyle get titleSmBold =>
+      const TextStyle(fontSize: 16, fontWeight: FontWeight.bold);
+
+  @override
+  TextStyle get bodyMdMedium =>
+      const TextStyle(fontSize: 12, fontWeight: FontWeight.w500);
+
+  @override
+  TextStyle get titleMdMedium =>
+      const TextStyle(fontSize: 14, fontWeight: FontWeight.w500);
+
+  @override
+  TextStyle get bodyLgBold =>
+      const TextStyle(fontSize: 14, fontWeight: FontWeight.bold);
+
+  @override
+  TextStyle get bodyLgMedium =>
+      const TextStyle(fontSize: 14, fontWeight: FontWeight.w500);
+
+  @override
+  TextStyle get titleLgBold =>
+      const TextStyle(fontSize: 24, fontWeight: FontWeight.bold);
+}
