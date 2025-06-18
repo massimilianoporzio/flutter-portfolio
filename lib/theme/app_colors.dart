1e13e7bc29fe9fb84f1cfb917e7a2f428bc86739
Massimiliano Porzio <massimiliano.porzio@gmail.com>
2025-06-18 18:18:37 +0200

    aggiunto colori

---
+++ lib/theme/app_colors.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const primaryColor = Color(0xFF561895);
+  static const darkBackgroundColor = Color(0xff110D12);
+  static const gray = MaterialColor(100, {
+    100: Color(0xffFBFBFB),
+    200: Color(0xFFE2e2e2),
+    300: Color(0xFFc9c9c9),
+    400: Color(0xFFb0b0b0),
+    500: Color(0xFF979797),
+    600: Color(0xFF7e7e7e),
+    700: Color(0xFF646464),
+    800: Color(0xFF333333),
+    900: Color(0xFF110d12),
+  });
+}
