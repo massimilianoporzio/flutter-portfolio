3f5319a507e006e35ea8e985c1ca88eb461d7d55
Massimiliano Porzio <massimiliano.porzio@gmail.com>
2025-06-20 23:51:30 +0200

    messo switch più carino

--- lib/widgets/appbar/app_bar_theme_switch.dart
+++ lib/widgets/appbar/app_bar_theme_switch.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_switch/flutter_switch.dart';
+import 'package:portfolio/common/extensions.dart';
 import 'package:portfolio/features/theme/presentation/cubit/theme_cubit.dart';
+import 'package:portfolio/theme/app_colors.dart';
 
 class ThemeToggle extends StatelessWidget {
   const ThemeToggle({super.key});
@@ -8,14 +11,41 @@ class ThemeToggle extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final themeMode = context.watch<ThemeCubit>().currentTheme;
-    return Switch(
-      value: themeMode == ThemeMode.dark,
-      onChanged: (value) {
-        print('Theme changed to: ${value ? 'Dark' : 'Light'}');
-        context.read<ThemeCubit>().changeTheme(
-          value ? ThemeMode.dark : ThemeMode.light,
-        );
-      },
+    return Padding(
+      padding: EdgeInsets.symmetric(horizontal: context.insets.padding),
+      child: FlutterSwitch(
+        value: themeMode == ThemeMode.dark,
+        width: 85,
+        height: 40.0,
+        toggleSize: 30.0,
+
+        activeToggleColor: Color(0xFF6E40C9),
+        inactiveToggleColor: AppColors.gray[800]!,
+        activeSwitchBorder: Border.all(
+          color: AppColors.primaryColor,
+          width: 6.0,
+        ),
+        inactiveSwitchBorder: Border.all(
+          color: Color(0xFFD1D5DA),
+          width: 6.0,
+        ),
+        activeColor: Color(0xFF271052),
+        inactiveColor: Colors.white,
+        activeIcon: Icon(
+          Icons.nightlight_round,
+          color: Color(0xFFF8E3A1),
+        ),
+        inactiveIcon: Icon(
+          Icons.wb_sunny,
+          color: Color(0xFFFFDF5D),
+        ),
+        onToggle: (value) {
+          print('Theme changed to: ${value ? 'Dark' : 'Light'}');
+          context.read<ThemeCubit>().changeTheme(
+            value ? ThemeMode.dark : ThemeMode.light,
+          );
+        },
+      ),
     );
   }
 }
